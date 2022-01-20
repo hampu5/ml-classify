8c363122077b7b33208962a1b7f57b72e68cc1bf
hampu5 <halu1701@student.miun.se>
2022-01-21 00:42:27 +0100

    Have a working script, but it takes too much time to fit the random forest model

--- ml_classify/study-dataset.py
+++ ml_classify/study-dataset.py
@@ -1,7 +1,11 @@
 import pandas as pd
 import numpy as np
 from collections import Counter
+from sklearn.ensemble import RandomForestClassifier
 from sklearn.feature_selection import SelectKBest, chi2
+from sklearn.metrics import f1_score
+from sklearn.model_selection import cross_val_score, train_test_split
+
 
 datasets = {}
 
@@ -9,51 +13,45 @@ datasets = {}
 # ORNL DATA
 PATH = "/home/hampus/miun/master_thesis/Datasets/ORNL/"
 
-ambient = pd.read_csv( f"{PATH}/ambient.csv" )
-attack = pd.read_csv( f"{PATH}/attack.csv" )
+ambient = pd.read_csv(f"{PATH}/ambient.csv")
+attack = pd.read_csv(f"{PATH}/attack.csv")
 
-
-ambient["filename"] = ambient["filename"].apply( lambda x: PATH + x)
-attack["filename"] = attack["filename"].apply( lambda x: PATH + x)
-
+ambient["filename"] = ambient["filename"].apply(lambda x: PATH + x)
+attack["filename"] = attack["filename"].apply(lambda x: PATH + x)
 
 ambient["has_attacks"] = False
 attack["has_attacks"] = True
 
-ambient = ambient[ ["name", "filename", "has_attacks"] ]
-attack =  attack[ ["name", "filename", "has_attacks"] ]
+ambient = ambient[["name", "filename", "has_attacks"]]
+attack =  attack[["name", "filename", "has_attacks"]]
 
-df1 = pd.concat( [ ambient, attack])
+df1 = pd.concat([ambient, attack])
 df1["remarks"] = "No DLC available"
 datasets["ORNL"] = df1.to_dict("records")
 
+# Release memory
+ambient = None
+attack = None
 
-#Features related to time
 
 # various functions to get the data into the format we want
 def calc_relative_time(AT):    
-    """helper function to translate absolute time to relative time"""
-    
+    # helper function to translate absolute time to relative time
     dt = AT.diff()
     dt[0] = dt.mean() # fill the missing first element
     return dt
 
 
 def calc_relative_time_per_id(df):
-    
-    df["dt_ID"] = df.groupby(by = "ID")["t"].diff()
-    
-
-    nans = df["dt_ID"].index[df["dt_ID"].apply(np.isnan)]
+    df["dt_ID"] = df.groupby(by="ID")["t"].diff()
 
     nans_idx = df["dt_ID"].index[df["dt_ID"].apply(np.isnan)]
-    nans_ids = [int(df.iloc[d]["ID"]) for d in nans_idx ]
+    nans_ids = [int(df.iloc[d]["ID"]) for d in nans_idx]
 
     meanall = df["dt_ID"].mean() # needed when an ID is used only once, hence no mean
-    means_ = df.groupby(by = "ID")["dt_ID"].mean().fillna(meanall).to_dict() # mean for each ID
-    nans_vals = [ means_[id_] for id_ in nans_ids]
+    means_ = df.groupby(by="ID")["dt_ID"].mean().fillna(meanall).to_dict() # mean for each ID
+    nans_vals = [means_[id_] for id_ in nans_ids]
     
-
     tmp = df["dt_ID"].copy()
     tmp.iloc[nans_idx] = nans_vals
     df["dt_ID"] = tmp
@@ -64,33 +62,32 @@ def read_file(filename):
     df = pd.read_csv(filename)
     
     if "Timestamp" in df.columns:
-        df.rename(columns = {'Timestamp':'t'}, inplace = True)
+        df.rename(columns={'Timestamp':'t'}, inplace=True)
     
-    df["dt"] =  calc_relative_time(df["t"])
+    df["dt"] = calc_relative_time(df["t"])
     calc_relative_time_per_id(df)
     
     return df
 
 
-df_attack=pd.DataFrame()
-df_ambient=pd.DataFrame()
+df_attack = pd.DataFrame()
+df_ambient = pd.DataFrame()
 
 for dname, dataset in datasets.items():
     for dataitem in dataset:
         name = dataitem["name"]
         filename = dataitem["filename"]
-        has_attacks = bool( dataitem["has_attacks"] )
+        has_attacks = bool(dataitem["has_attacks"])
         remarks = dataitem["remarks"] or ""
         
-         
         if has_attacks:             
             df = read_file(filename)
             df_attack = df_attack.append(df, ignore_index=True)
-        else:             
-            df = read_file(filename)
-            df_ambient = df_ambient.append(df, ignore_index=True)    
-                  
+        # else:             
+        #     df = read_file(filename)
+        #     df_ambient = df_ambient.append(df, ignore_index=True)    
 
+df_ambient = None # Release memory, as it isn't used for now
 
 # print(df_ambient)
 # print(df_attack)
@@ -99,14 +96,13 @@ for dname, dataset in datasets.items():
 
 def get_labels_by_class(class_, window_size): 
     # return [c for c in df.columns if c.startswith(class_ + "_")]    
-    return [ f"{class_}_{i}" for i in range(window_size) ]
+    return [f"{class_}_{i}" for i in range(window_size)]
 
 #Features related to frequency using window
 def extract_window(df, window_size):
-
     cs = df.columns
     dfw = pd.DataFrame()
-    id_column_names =[] #added
+    id_column_names = [] #added
     for i in range(window_size):
         tmp = df.shift(-i)
         column_names = {c: f"{c}_{i}" for c in cs}
@@ -119,44 +115,70 @@ def extract_window(df, window_size):
     
     print('ID NAMES: %s' % id_column_names) #added
 
-    
-
     ## Calculate
     dfw["win_most_freq_id"] = dfw.apply(lambda x: list([x[name] for name in id_column_names]), axis=1) #added
     dfw["win_most_freq_id"]= dfw["win_most_freq_id"].apply(lambda x: max(Counter(x).values())) #added
-
-
     
     # drop the last ones
     len_ = dfw.shape[0]
-    dfw.drop( range(len_ - window_size, len_), inplace = True)
+    dfw.drop(range(len_ - window_size, len_), inplace=True)
 
     # compute the labels and remove the individual labels    
     lbs = get_labels_by_class("Label", window_size)    
-    dfw["Label"] = dfw[lbs].max( axis = 1)
-    dfw.drop(lbs, axis = 1, inplace = True)
+    dfw["Label"] = dfw[lbs].max(axis=1)
+    dfw.drop(lbs, axis=1, inplace=True)
     
     # compute win_dir and win_mean_dt:
     dts = get_labels_by_class("dt", window_size)    
-    dfw["win_dur"] = dfw[dts].sum( axis = 1)
-    dfw["win_mean_dt"] = dfw[dts].mean( axis = 1)
+    dfw["win_dur"] = dfw[dts].sum(axis=1)
+    dfw["win_mean_dt"] = dfw[dts].mean(axis=1)
     
     # drop the absolut times, they are useless now
     ts = get_labels_by_class("t", window_size)    
-    dfw.drop(ts, axis = 1, inplace = True)
-    
-    
+    dfw.drop(ts, axis=1, inplace=True)
+
     Y = dfw["Label"]
-    X = dfw.drop("Label", axis = 1)
+    X = dfw.drop("Label", axis=1)
+    
     return X, Y
 
 
-window_size=10
-attack_X,attack_Y = extract_window(df_attack, window_size)
+# Using Rosell to get frequency-based features from df with attacks in it
+window_size = 10
+attack_X, attack_Y = extract_window(df_attack, window_size)
 print(attack_X.shape)
 
 
 # feature selection:
-features=100
-selector = SelectKBest(chi2, k = features)
-print("KBest Selected")
+features = 100
+selector = SelectKBest(chi2, k=features)
+print("K Best Selected!")
+
+X_train, X_test, y_train, y_test = train_test_split(attack_X, attack_Y, test_size=0.5, random_state=2, shuffle=True)
+print("Test and training data created!")
+
+X_train = selector.fit_transform(X_train, y_train)
+print("Training data fitted!")
+
+# Free memory
+attack_X = None
+attack_Y = None
+
+X_test = selector.fit_transform(X_test, y_test)
+print("Test data fitted!")
+
+
+#Classification with Random Forest
+clf = RandomForestClassifier(n_estimators=100)
+clf.fit(X_train, y_train)
+print("Random Forest model fitted!")
+
+scores = cross_val_score(clf, X_train, y_train, scoring='f1', cv=10)
+print("Training F1: %0.4f (+/- %0.4f)" % (scores.mean(), scores.std()))
+
+pred = clf.predict(X_test)
+print("Test data has been Classified!")
+
+f1_scores = f1_score(y_test, pred, average='weighted')
+print("Testing F1:  %0.4f(+/- %0.4f)" % (f1_scores.mean(), f1_scores.std()))
+   
