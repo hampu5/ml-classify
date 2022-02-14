3878daf0dd50b5860d313e7c5d58e426cb5dc735
hampu5 <halu1701@student.miun.se>
2022-02-14 14:54:50 +0100

    added a file to check the dataset for max val and such

---
+++ ml_classify/check-dataset.py
@@ -0,0 +1,17 @@
+import numpy as np
+import pandas as pd
+
+
+path = "/home/hampus/miun/master_thesis/Datasets/Hisingen/"
+
+df1 = pd.read_csv(path + "data.csv")
+
+df_attack = pd.DataFrame()
+for index, dataitem in df1.iterrows():
+    name = dataitem["name"]
+    filename = dataitem["filename"]
+    
+    df = pd.read_csv(path + filename)
+    print(name)
+    # print(df)
+    print(np.amax(df["d0"]))
--- ml_classify/explain-data.py
+++
@@ -1,9 +0,0 @@
-import shap
-from joblib import load
-
-shap_values = load("RF_ROAD_Shap.joblib")
-print(shap_values)
-# tmp = shap.Explanation(shap_values[:, :, 1])
-
-
-# shap.plots.beeswarm(tmp)
