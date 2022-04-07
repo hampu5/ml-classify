ed1c652cf35032ded5b8ae00ea52ef46e65b62c6
hampu5 <halu1701@student.miun.se>
2022-04-07 15:27:48 +0200

    trying..

--- ml_classify/nn.py
+++ ml_classify/nn.py
@@ -1,70 +1,52 @@
 import numpy as np
 
-def sigmoid(x): 1/(1 + np.exp(-x))
+def sigmoid(x, deriv=False):
+    if deriv:
+        return x*(1-x)
+    return 1/(1 + np.exp(-x))
 
-def relu(x): np.maximum(x, 0, x)
+def relu(x, deriv=False):
+    if deriv:
+        return np.greater(x, 0).astype(int)
+    return np.maximum(x, 0)
 
-class Network(object):
-    def __init__(self, *args, **kwargs):
-        input_size = kwargs.get('input_size', None)
-        hidden_size = kwargs.get('hidden_size', None)
-        output_size = kwargs.get('output_size', None)
-        self.weights = [
-            np.random.rand(input_size, hidden_size).T,
-            np.random.rand(hidden_size, output_size).T
-        ]
-        self.biases = [
-            np.zeros(hidden_size),
-            np.zeros(output_size)
-        ]
-        self.activation = kwargs.get('activation', sigmoid)
-        # self.activation_derivative = kwargs.get('activation_derivative', sigmoid)
-    
-    def feedforward(self, a):
-        """Return the output of the network for an input vector a"""
-        for w, b in zip(self.weights, self.biases):
-            a = np.dot(w, a) + b
-            # print(a)
-            self.activation(a)
-        return a
-    
-    def backpropagation(self, x, y):
-        """Return a list of gradients for each layer"""
-        gradients = []
-        # feedforward
-        activations = [x]
-        for w, b in zip(self.weights, self.biases):
-            a = np.dot(w, activations[-1]) + b
-            activations.append(self.activation(a))
-        # backward pass
-        delta = activations[-1] - y
-        gradients.append(np.dot(delta, activations[-2].T) / len(x))
-        for l in range(2, len(self.weights) + 1):
-            delta = np.dot(self.weights[-l + 1].T, delta) * self.activation_derivative(activations[-l])
-            gradients.append(np.dot(delta, activations[-l - 1].T) / len(x))
-        return gradients
-    
-    def fit(self, X, y, epochs=10, learning_rate=0.1):
-        """Fit the network to the training data"""
-        for epoch in range(epochs):
-            for x, y in zip(X, y):
-                activations = [x]
-                for w, b in zip(self.weights, self.biases):
-                    a = np.dot(w, activations[-1]) + b
-                    activations.append(self.activation(a))
-                gradients = self.backpropagation(activations, y)
-                for w, g in zip(self.weights, gradients):
-                    w -= learning_rate * g
+num_epochs = 60000
+
+#initialize weights
+syn0 = 2*np.random.random((4,3)) - 1
+syn1 = 2*np.random.random((3,1)) - 1
 
 
-
-X_train = np.array([0.1, 0.1, 0.1, 0.1])
+X_train = np.array([1, 1, 0, 0])
 y_train = np.array([1])
 
-nn = Network(input_size=4, hidden_size=3, output_size=1, activation=sigmoid)
 
-print(X_train)
+#Step 3 Train Model
 
-data = nn.fit(X_train, y_train)
-
-print(data)
+for j in range(num_epochs):
+    #feed forward through layers 0,1, and 2
+    k0 = X_train
+    k1 = sigmoid(np.dot(k0, syn0))
+    k2 = sigmoid(np.dot(k1, syn1))
+    
+    #how much did we miss the target value?
+    k2_error = y_train - k2
+    
+    if (j% 10000) == 0:
+        print("Error:" + str(np.mean(np.abs(k2_error))))
+    
+    #in what direction is the target value?
+    k2_delta = k2_error * sigmoid(k2, deriv=True)
+    
+    #how much did each k1 value contribute to k2 error
+    k1_error = np.dot(k2_delta, syn1.T)
+    
+    k1_delta = k1_error * sigmoid(k1, deriv=True)
+    
+    print(k1)
+    print(k2_delta)
+    print(syn1)
+    asd = np.dot(k1, k2_delta[0])
+    print(asd)
+    syn1 += asd.T
+    syn0 += np.dot(k0, k1_delta)
